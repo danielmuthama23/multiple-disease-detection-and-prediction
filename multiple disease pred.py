ed781fba3025ba930ea4bde787191898f2014607
ProfLaura <danielmuthama23@gmail.com>
2023-11-23 23:55:38 +0300

    update files

--- multiple disease pred.py
+++ multiple disease pred.py
@@ -7,9 +7,7 @@ from streamlit_option_menu import option_menu
 # loading the saved models
 
 diabetes_model = pickle.load(open('C:/Users/hp/project/saved models/diabetes_model.sav', 'rb'))
-
 heart_disease_model = pickle.load(open('C:/Users/hp/project/saved models/heart_disease_model.sav','rb'))
-
 parkinsons_model = pickle.load(open('C:/Users/hp/project/saved models/parkinsons_model.sav', 'rb'))
 
 
